2110d2c91c1713c9a09e9acf6d68c7733b954ba5
calro999 <gamingmedia.mail@gmail.com>
2026-08-01 02:18:58 +0900

    表示改善

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  outputFileTracingRoot: __dirname,
   trailingSlash: true,
   images: {
     unoptimized: true, // 静的エクスポート時には画像の自動最適化を無効化する
-  },
-  experimental: {
-    turbo: {
-      root: __dirname,
-    },
   },
 };
 
